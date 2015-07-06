631260b0118c2e10b3e15055a5cb6b1f0b8556e9
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2015-07-06 14:25:57 +0200

    PLAT-121: 'grunt:compound' generates skeleton for compound-component

--- generators/app/index.js
+++ generators/app/index.js
@@ -12,6 +12,7 @@ module.exports = yeoman.generators.Base.extend({
         this.log(yosay(
             'Welcome to the neat ' + chalk.red('Cubixx WebPackage') +
             ' generator!' + '\n' + 'Please specify the type of webpackage you\'d like to create:' +
+            '\nyo cubixx:compound (for a Compound Cubixx Component)'+
             '\nyo cubixx:elementary (for a Elementary Cubixx Component)'+
             '\nyo cubixx:webutil (\'webutil\' is also known as \'webresource\')'
         ));
---
+++ generators/compound/templates/tasks/configs/pack.js
@@ -0,0 +1,64 @@
+/*jshint node: true */
+/*global module */
+'use strict';
+
+module.exports.tasks = {
+    copy: {
+        backgroundJs: {
+            files: [
+                {
+                    expand: true,
+                    cwd: '<%= param.src %>/',
+                    src: 'js/background.js',
+                    dest: '<%= param.dst %>/'
+                }]
+        },
+        installPage: {
+            files: [
+                {
+                    expand: true,
+                    cwd: 'helper/',
+                    src: 'install.html',
+                    dest: '<%= param.pack %>/'
+                }]
+        }
+    },
+    clean: {
+        pack: ['<%= param.dst %>/test']
+    },
+    fileExists: {
+        pack: [
+            '<%= param.dst %>/manifest.webpackage',
+            '<%= param.dst %>/manifest.cubx',
+            '<%= param.dst %>/dependency.json',
+            '<%= param.dst %>/manifest.component',
+            '<%= param.dst %>/style/app.min.css'
+        ]
+    },
+    'string-replace': {
+        crc_url: {
+            files: [
+                {
+                    expand: true,
+                    cwd: '<%= param.dst %>/',
+                    src: 'index.html',
+                    dest: '<%= param.dst %>/'
+                }],
+            options: {
+                replacements: [
+                    {
+                        //if developed directly against database (deprecated)
+                        //e.g. http://boot2docker.me:5984/webpackage-store/crc-loader...
+                        pattern: /(https?:\/\/[\w\.\-]*:\d{0,4}\/[\w\d\.\-]*\/)(crc-loader)/ig,
+                        replacement: '../$2'
+                    },
+                    {
+                        //if developed against the gateway (recommended)
+                        //e.g. http://boot2docker.me/crc-loader...
+                        pattern: /(https?:\/\/[\w\.\-]*\/)(crc-loader)/ig,
+                        replacement: '/$2'
+                    }]
+            }
+        }
+    }
+};
