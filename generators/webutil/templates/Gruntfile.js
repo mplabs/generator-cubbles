8de70bf24e0be6050540c0ef1cf3b8079bde7619
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2015-07-07 13:33:55 +0200

    PLAT-201: spell correction in Gruntfile.js

--- generators/webutil/templates/Gruntfile.js
+++ generators/webutil/templates/Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask('initGit', 'Explains how to setup a git repo for this webpackage.', function() {
         grunt.log.writeln('\n' +
-            'We don`t find it usesful to automate this for you - but here is what you need to do:');
+            'We don`t find it useful to automate this for you - but here is what you need to do:');
         grunt.log.writeln(chalk.green('$ git init') +
             ' //use \'--template <path>\' to use a git template of your choice');
         grunt.log.writeln(chalk.green('$ git remote set-url origin <url>') +
