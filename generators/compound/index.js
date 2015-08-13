8b1ab7c15f05c04d5cd35a8e07a660df83f3f777
Judit Ross <jtrs@incowia.de>
2015-08-13 11:08:38 +0200

    PLAT-200: regex für groupId angepasst

--- generators/compound/index.js
+++ generators/compound/index.js
@@ -10,7 +10,7 @@ module.exports = yeoman.generators.Base.extend({
          * regex definitions for input validation (note: use http://regexpal.com/ for testing)
          */
         this.webpackagename_regex = /^([a-z][a-z0-9]*)(\-[a-z0-9]+)+$/;
-        this.groupid_regex = /^([a-z0-9_-]+)(\.[a-z0-9_-]+)*$/;
+        this.groupid_regex = /^([a-z0-9]+||([a-z0-9]+[a-z0-9-][a-z0-9]+)*)(\.([a-z0-9]+||([a-z0-9]+[a-z0-9-][a-z0-9]+)*))*$/;
         this.version_regex = /^(\d+)(\.[\d]+)*(-SNAPSHOT)?$/;
         this.peopleName_regex = /^(([A-Za-zäöüÄÖÜ]+[\-\']?)*([A-Za-zäöüÄÖÜ]+)?[\.]?\s)+([A-Za-zäöüÄÖÜ]+[\-\']?)*([A-Za-zäöüÄÖÜ]+)?$/;
         // for the url-regex @see https://mathiasbynens.be/demo/url-regex
