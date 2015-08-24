b27d90d9b1ac651772e709dd3a7608ca837d2058
Judit Ross <jtrs@incowia.de>
2015-08-24 15:17:17 +0200

    PLAT-264:  BugFix pollyfill_io.js use strict zugefügt

--- generators/webapp/templates/public/js/polyfill_ie.js
+++ generators/webapp/templates/public/js/polyfill_ie.js
@@ -1,9 +1,9 @@
-(function () {
+(function() {
     'use strict';
-    function CustomEvent ( event, params ) {
-        params = params || { bubbles: false, cancelable: false, detail: undefined };
-        var evt = document.createEvent( 'CustomEvent' );
-        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    function CustomEvent(event, params) {
+        params = params || {bubbles: false, cancelable: false, detail: undefined};
+        var evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
         return evt;
     }
 
